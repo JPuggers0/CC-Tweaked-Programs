d14dcecae0231df1cc30922012af03da1b4c253b
JPuggers0 <60408957+JPuggers0@users.noreply.github.com>
2026-05-15 12:01:58 -0400

    quick fix (I hope)

--- Mandatory Setup Programs/Base Setup.lua
+++ Mandatory Setup Programs/Base Setup.lua
@@ -2,7 +2,8 @@
 shell.run(
     "wget",
     "run",
-    "https://raw.githubusercontent.com/JPuggers0/CC-Tweaked-Programs/master/Mandatory%20Setup%20Programs/Basalt_installer.lua"
+    "https://raw.githubusercontent.com/JPuggers0/CC-Tweaked-Programs/master/Mandatory%20Setup%20Programs/Basalt_installer.lua",
+    "-f"
 )
 
 local software = {
