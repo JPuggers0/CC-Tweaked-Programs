e8a72cb70c8187084d3f2159cdbfa11a2e4d38c4
JPuggers0 <60408957+JPuggers0@users.noreply.github.com>
2026-05-15 15:29:33 -0400

    fixes

--- Mandatory Setup Programs/Base Setup.lua
+++ Mandatory Setup Programs/Base Setup.lua
@@ -145,7 +145,7 @@ display:onClick(function(self, btn, x, y)
 end)
 
 local function doInstall()
-    if installing then return end
+    basalt.stop()
     local toInstall = {}
     for i, sw in ipairs(software) do
         if checked[i] then
@@ -153,16 +153,15 @@ local function doInstall()
         end
     end
     if #toInstall == 0 then
-        return
+        print("No software selected.")
+    else
+        for _, sw in ipairs(toInstall) do
+            print("Installing: " .. sw.name)
+            shell.run("wget", "run", sw.url)
+        end
+        print("Done!")
+        rebootWithMessage()
     end
-    installing = true
-    basalt.stop()
-    for _, sw in ipairs(toInstall) do
-        print("Installing: " .. sw.name)
-        shell.run("wget", "run", sw.url)
-    end
-    print("Done!")
-    rebootWithMessage()
 end
 
 basalt.onEvent("key", function(key)
